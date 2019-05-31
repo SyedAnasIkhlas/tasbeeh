93ec672fa6d4b9fb18b82355d58f49b17699e2a0
Syed Anas Ikhlas <syedanasikhlas2050@gmail.com>
2019-05-31 13:43:23 +0300

    change numbers font to digits fonts and add a color effect on button clicked for 0.1 seconds

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,26 +1,45 @@
-function reset(s)
+function reset()
 {
 	var numbers = $("#numbers").val();
 	var num = parseInt(numbers);
 	var reset = num * 0;
-	$(".reset_button").effect( "shake" );
-	navigator.vibrate(s);
+	navigator.vibrate(200);
+
+	var $el = $(".reset_button"),
+    x = 100,
+    originalColor = $el.css("background");
+
+	$el.css("background", "#77ff7b");
+	setTimeout(function(){
+	  $el.css("background", originalColor);
+	}, x);
+
 	return $("#numbers").val(reset);
 
 }
 
-function addOne(s)
+function addOne()
 {
 	var numbers = $("#numbers").val();
 	var num = parseInt(numbers);
 	var addOne = num + 1;
-	navigator.vibrate(s);
-	$(".add_button").effect( "shake" );
+	navigator.vibrate(200);
+
+	var $el = $(".add_button"),
+    x = 100,
+    originalColor = $el.css("background");
+
+	$el.css("background", "#77ff7b");
+	setTimeout(function(){
+	  $el.css("background", originalColor);
+	}, x);
+
 	return $("#numbers").val(addOne);
+
 	
 }
 
-function deduct(s)
+function deduct()
 {
 	var numbers = $("#numbers").val();
 	var num = parseInt(numbers);
@@ -33,8 +52,17 @@ function deduct(s)
 	{
 		var deduct = num - 1;
 	}
-	$(".deduct_button").effect( "shake" );
-	navigator.vibrate(s);
+	navigator.vibrate(200);
+
+	var $el = $(".deduct_button"),
+    x = 100,
+    originalColor = $el.css("background");
+
+	$el.css("background", "#77ff7b");
+	setTimeout(function(){
+	  $el.css("background", originalColor);
+	}, x);
+
 	return $("#numbers").val(deduct);
 	
 }
