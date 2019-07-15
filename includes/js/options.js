88f992a0290d6e301cdb87d8d71b277a7ea26efa
Syed Anas Ikhlas <syedanasikhlas2050@gmail.com>
2019-07-15 11:11:24 +0300

    dark theme

---
+++ includes/js/options.js
@@ -0,0 +1,71 @@
+
+function options() 
+{
+    $(".useful-options").toggleClass("display");
+
+}
+
+
+$(document).ready(function()
+{
+
+	if (localStorage.getItem("checked") == null) 
+	{
+		//noting is checked
+	}
+	else
+	{
+		
+		$( 'input[type="checkbox"]' ).prop( "checked", true );
+		$("body").addClass('drake-mode');
+        $(".tasbeeh").addClass('drake-mode');
+        $(".target").addClass('drake-mode');
+        $("textarea").addClass('drake-mode');
+		$(".nav").addClass('drake-mode');
+        $("footer").addClass('drake-mode');
+        $(".target-msg").addClass('drake-mode');
+        $(".contact").addClass('drake-mode');
+        $("#options").addClass('drake-mode');
+        $(".menu").addClass('drake-mode');
+        $(".classic-tasbeeh-view").addClass('drake-mode');
+
+
+	}
+
+        $('input[type="checkbox"]').click(function()
+        {
+            if($(this).prop("checked") == true)
+            {
+                localStorage.setItem("checked","1")
+                $("body").addClass('drake-mode');
+                $(".tasbeeh").addClass('drake-mode');
+                $(".target").addClass('drake-mode');
+                $("textarea").addClass('drake-mode');
+				$(".nav").addClass('drake-mode');
+                $("footer").addClass('drake-mode');
+                $(".target-msg").addClass('drake-mode');
+                $(".contact").addClass('drake-mode');
+                $("#options").addClass('drake-mode');
+                $(".menu").addClass('drake-mode');
+                $(".classic-tasbeeh-view").addClass('drake-mode');
+
+            }
+            else if($(this).prop("checked") == false)
+            {
+                localStorage.removeItem("checked")
+                $("body").removeClass('drake-mode');
+                $(".tasbeeh").removeClass('drake-mode');
+                $(".target").removeClass('drake-mode');
+                $("textarea").removeClass('drake-mode');
+				$(".nav").removeClass('drake-mode');
+                $("footer").removeClass('drake-mode');
+                $(".target-msg").removeClass('drake-mode');
+                $(".contact").removeClass('drake-mode');
+                $("#options").removeClass('drake-mode');
+                $(".menu").removeClass('drake-mode');
+                $(".classic-tasbeeh-view").removeClass('drake-mode');
+            }
+        });
+    });
+
+	
