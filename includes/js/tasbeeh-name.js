636026fb56fc09dd2f1f3bba126757baaa82ea83
Syed Anas Ikhlas <syedanasikhlas2050@gmail.com>
2019-08-10 09:45:29 +0300

    fix black brand name

--- includes/js/tasbeeh-name.js
+++ includes/js/tasbeeh-name.js
@@ -2,8 +2,8 @@ const defaultBrandName = "<b>T</b>asbeeh <b>C</b>ounter</span>";
 
 function setBrandToDefault()
 {
-	$(".logo a").html(localStorage.getItem('headerBrandName'));
-	$(".brand_name").html(localStorage.getItem('tabeehBrandName'));
+	$(".logo a").html("Tasbeeh Counter");
+	$(".brand_name").html("<b>T</b>asbeeh <b>C</b>ounter</span>");
 	$(".brand_name").removeClass('custom-brand-font');
 }
 
@@ -79,6 +79,7 @@ $(".brand-name").keyup(function()
 		}
 		if($(".brand-name").val() != "")
 		{
+			$(".brand_name").addClass('custom-brand-font');
 			$(".brand_name").html($(".brand-name").val());
 			$(".logo a").html($(".brand-name").val());
 		}
