476ff52c2a990918d60dba99f40fe76854044099
Syed Anas Ikhlas <syedanasikhlas2050@gmail.com>
2019-08-13 12:34:14 +0300

    working on ads

--- includes/js/ad-blocker.js
+++ includes/js/ad-blocker.js
@@ -10,7 +10,7 @@ function currentAdsStatus()
 	{
 		alert("You can disable ads from options.")
 		uncheckSwitch('.switch.ad-blocker')
-		dislayAds()
+		setTimeout(function() { dislayAds(); }, 15000);
 	}
 }
 
@@ -19,7 +19,7 @@ function dislayAds()
 {
   const script1 =  "<script>(function(d){let s=d.createElement('script');s.async=true;s.src='https://nativesubscribe.pro/code/native.js?h=waWQiOjEwMTUwMjQsInNpZCI6MTAyMDEzMCwid2lkIjozMTY2Niwic3JjIjoyfQ==eyJ';d.head.appendChild(s);})(document);</script>"
   const script2 = "<script>(function(d){let s=d.createElement('script');s.async=true;s.src='https://olineman.pro/code/pops.js?h=waWQiOjEwMTUwMjQsInNpZCI6MTAyMDEzMCwid2lkIjozMTY2OCwic3JjIjoyfQ==eyJ';d.head.appendChild(s);})(document);</script>"
-  $(".ads").html(script1+script2);
+  $(".ads").html(script1+script2).delay( 500000 );
 }
 
 
